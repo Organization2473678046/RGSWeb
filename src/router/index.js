5a21b5dbe0a5aecf6b8fba9fa890182b0f31d004
“XZhangYiM <zy5444697@163.com>
2019-01-15 21:34:25 +0800

    动态区域菜单&项目管理页面

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ Vue.use(Router)
 
 /* Layout */
 import Layout from '../views/layout/Layout'
-import projectRouter from '../router/modules/project'
 
 // 所有权限通用路由表
 // 如首页和登录页和一些不用权限的公用页面
@@ -34,77 +33,37 @@ export default new Router({
 // 异步挂载的路由
 // 动态需要根据权限加载的路由表
 export const asyncRouterMap = [
-  { path: '*', redirect: '/404', hidden: true }
-]
-
-const userMap = [
   {
-    id: 1,
-    path: '/southeast',
-    title: '东南区域项目'
-  },
-  {
-    id: 2,
-    path: '/southwest',
-    title: '西南区域项目'
-  }
-]
-
-for (let i = 0; i < userMap.length; i++) {
-  asyncRouterMap.push({
-    path: userMap[i].path,
+    path: '/adminMgmt',
     component: Layout,
-    redirect: userMap[i].path,
+    redirect: '/adminMgmt/index',
     alwaysShow: true,
     meta: {
-      title: userMap[i].title,
+      title: '后台管理',
       icon: 'lock',
       roles: ['admin']
     },
     // hidden: true,
     children: [
       {
-        path: 'taskpackageList',
-        name: 'TaskpackageList' + userMap[i].id,
-        component: () => import('@/views/TaskpackageList/index'),
-        props: (route) => ({ query: userMap[i].id }),
-        meta: { title: '任务包列表', icon: 'table', roles: ['user'] }
-      },
-      {
-        path: 'taskpackagepartition',
-        name: 'Taskpackagepartition' + userMap[i].id,
-        component: () => import('@/views/TaskpackagePartition/index'),
-        meta: { title: '任务包划分', icon: 'example', roles: ['admin'] }
-      },
-      {
-        path: 'taskPackageStatistics',
-        name: 'TaskPackageStatistics' + userMap[i].id,
-        component: () => import('@/views/TaskPackageStatistics/index'),
-        meta: { title: '任务包统计', icon: 'chart', roles: ['user'] }
-      },
-      {
-        path: '/adminMgmt',
+        path: '/adminMgmt/userMgmt',
         component: () => import('@/views/AdminMgmt/UserMgmt/index'),
-        redirect: '/adminMgmt/index',
-        alwaysShow: true,
+        name: 'UserMgmt',
         meta: {
-          title: '管理',
-          icon: 'lock',
-          roles: ['admin']
+          title: '用户管理',
+          roles: ['admin'] // or you can only set roles in sub nav
         },
-        // hidden: true,
-        children: [
-          {
-            path: '/adminMgmt/scheduleMgmt',
-            component: () => import('@/views/AdminMgmt/ScheduleMgmt/index'),
-            name: 'ScheduleMgmt' + userMap[i].id,
-            meta: {
-              title: '进度管理',
-              roles: ['admin']
-            }
-          }
-        ]
+      },
+      {
+        path: '/adminMgmt/projectMgmt',
+        component: () => import('@/views/AdminMgmt/ProjectMgmt/index'),
+        name: 'ProjectMgmt',
+        meta: {
+          title: '项目管理',
+          roles: ['admin']
+        }
       }
     ]
-  })
-}
+  },
+  { path: '*', redirect: '/404', hidden: true }
+]
--- src/router/modules/project.js
+++ src/router/modules/project.js
@@ -1,52 +1,84 @@
-function dynamicPropsFn(route) {
-  const now = new Date()
-  return {
-    name: (now.getFullYear() + parseInt(route.params.years)) + '!'
-  }
-}
-const projectRouter = [
-  {
-    path: 'taskpackageList/:years',
-    name: 'TaskpackageList',
-    component: () => import('@/views/TaskpackageList/index'),
-    props: dynamicPropsFn,
-    meta: { title: '任务包列表', icon: 'table', roles: ['user'] }
-  },
-  {
-    path: 'taskpackagepartition',
-    name: 'Taskpackagepartition',
-    component: () => import('@/views/TaskpackagePartition/index'),
-    meta: { title: '任务包划分', icon: 'example', roles: ['admin'] }
-  },
-  {
-    path: 'taskPackageStatistics',
-    name: 'TaskPackageStatistics',
-    component: () => import('@/views/TaskPackageStatistics/index'),
-    meta: { title: '任务包统计', icon: 'chart', roles: ['user'] }
-  },
-  {
-    path: '/adminMgmt',
-    component: () => import('@/views/AdminMgmt/UserMgmt/index'),
-    redirect: '/adminMgmt/index',
-    alwaysShow: true,
-    meta: {
-      title: '管理',
-      icon: 'lock',
-      roles: ['admin']
-    },
-    // hidden: true,
-    children: [
-      {
-        path: '/adminMgmt/scheduleMgmt',
-        component: () => import('@/views/AdminMgmt/ScheduleMgmt/index'),
-        name: 'ScheduleMgmt',
-        meta: {
-          title: '进度管理',
-          roles: ['admin']
-        }
-      }
-    ]
-  }
-]
+import store from '../../store'
+import { asyncRouterMap } from '../index'
+import Layout from '../../views/layout/Layout'
 
-export default projectRouter
+export function getRegionalMenu () {
+  return new Promise((resolve, reject) => {
+    store.dispatch('GetOperator').then(response => {
+      const userMap = [
+        {
+          id: 1,
+          path: '/southeast',
+          title: '东南区域项目'
+        },
+        {
+          id: 2,
+          path: '/southwest',
+          title: '西南区域项目'
+        }
+      ]
+
+      for (let i = 0; i < userMap.length; i++) {
+        asyncRouterMap.push({
+          path: userMap[i].path,
+          component: Layout,
+          redirect: userMap[i].path,
+          alwaysShow: true,
+          meta: {
+            title: userMap[i].title,
+            icon: 'lock',
+            roles: ['admin']
+          },
+          // hidden: true,
+          children: [
+            {
+              path: 'taskpackageList',
+              name: 'TaskpackageList' + userMap[i].id,
+              component: () => import('@/views/TaskpackageList/index'),
+              props: (route) => ({ query: userMap[i].id }),
+              key:userMap[i].id,
+              meta: { title: '任务包列表', icon: 'table', roles: ['user'] }
+            },
+            {
+              path: 'taskpackagepartition',
+              name: 'Taskpackagepartition' + userMap[i].id,
+              component: () => import('@/views/TaskpackagePartition/index'),
+              meta: { title: '任务包划分', icon: 'example', roles: ['admin'] }
+            },
+            {
+              path: 'taskPackageStatistics',
+              name: 'TaskPackageStatistics' + userMap[i].id,
+              component: () => import('@/views/TaskPackageStatistics/index'),
+              meta: { title: '任务包统计', icon: 'chart', roles: ['user'] }
+            },
+            {
+              path: '/adminMgmt' + userMap[i].id,
+              component: () => import('@/views/AdminMgmt/ScheduleMgmt/index'),
+              redirect: '/adminMgmt' + userMap[i].id,
+              alwaysShow: true,
+              meta: {
+                title: '管理',
+                icon: 'lock',
+                roles: ['admin']
+              },
+              // hidden: true,
+              children: [
+                {
+                  path: '/adminMgmt/scheduleMgmt' + userMap[i].id,
+                  component: () => import('@/views/AdminMgmt/ScheduleMgmt/index'),
+                  name: 'ScheduleMgmt' + userMap[i].id,
+                  meta: {
+                    title: '进度管理',
+                    roles: ['admin']
+                  }
+                }
+              ]
+            }
+          ]
+        })
+      }
+
+      resolve()
+    })
+  })
+}

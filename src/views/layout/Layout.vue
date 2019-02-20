21acdf5a808578910a5358329dff123c9f23a18e
“XZhangYiM <zy5444697@163.com>
2019-02-20 08:37:57 +0800

    menuhide...

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,11 +1,12 @@
 <template>
   <div :class="classObj" class="app-wrapper">
-    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"/>
+    <!-- <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"/>
     <sidebar class="sidebar-container"/>
     <div class="main-container">
       <navbar/>
       <app-main/>
-    </div>
+    </div> -->
+    <app-main/>
   </div>
 </template>
 

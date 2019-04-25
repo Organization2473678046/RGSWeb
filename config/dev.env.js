3239c93f58d8eb05d9ea4c68845203a847e3c892
176177082 <176177082@qq.com>
2019-04-25 10:15:39 +0800

    将v0.10改为v0.11

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://192.168.3.120:8000/v0.10"',
+  BASE_API: '"http://192.168.23.139:8000/v0.11"',
   // BASE_API: '"http://192.168.3.111:8080/v0.10"',
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://192.168.3.120:8000/v0.10"',
+  BASE_API: '"http://192.168.23.139:8000/v0.11"',
 }
